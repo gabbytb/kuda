2777d4ba8a06f825ef34afe1a7921ab9631887f8
Gabby <igabriel.oyebanji@travelbeta.com>
2024-04-18 09:09:16 +0100

    Pushed Commit 18/04/24

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -39,9 +39,6 @@ const Nav = () => {
         <div className="mobile-toggle">
           <div className="hamburger">
             <div className="hamburger--inner"></div>
-        <div className="mobile-toggle">
-          <div className="hamburger">
-            <div className="hamburger--inner"></div>
           </div>
         </div>
     </header>
