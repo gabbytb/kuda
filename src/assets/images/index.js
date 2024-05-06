1e8d2bfc2bbf3aeb79c79d5ddeee101800dc4e9a
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-05-06 12:49:49 +0100

    Pushed to Repo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import BrandLogo from "./BrandLogo";
+
 import SectionOneImgBg from "./SectionOneImgBg";
 import SectionOneImg from "./SectionOneImg";
 import SectionThreeImg from "./SectionThreeImg";
@@ -7,6 +8,7 @@ import SectionFiveImg from "./SectionFiveImg";
 import SectionSixImg from "./SectionSixImg";
 import SectionSevenImg from "./SectionSevenImg";
 import SectionEightImg from "./SectionEightImg";
+
 import customer1 from "./muhammad_0d83f8fad33c4bef3b0e5dba3a7e9bb1.jpg";
 import customer3 from "./babajide_fd3e74acb541331ec1814659d52d93b8.jpg";
 import customer4 from "./realsok_c5ad605dc584730df0f6f32896e5aac7.jpg";
@@ -16,6 +18,7 @@ import customer5 from "./adebambo_054bc344c7eff7df24addc60817fa658.jpg";
 
 export {
     BrandLogo,
+    
     SectionOneImg,
     SectionOneImgBg,
     SectionThreeImg,
